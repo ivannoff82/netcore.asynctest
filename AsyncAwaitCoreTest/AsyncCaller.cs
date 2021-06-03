cff0f53c6a1c19cc62c8fca456e1eddeacf86aa9
Ivan Novokhatsky <inovokhatsky@bpmlab.ru>
2021-06-03 09:30:59 +0700

    Add async/await example

---
+++ AsyncAwaitCoreTest/AsyncCaller.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitCoreTest
+{
+    /// <summary>
+    /// Async demo class
+    /// </summary>
+    public class AsyncCaller
+    {
+        /// <summary>
+        /// Indexer of long operation
+        /// </summary>
+        public int Number;
+
+        /// <summary>
+        /// Is increase in progress
+        /// </summary>
+        public bool InProcess = false;
+
+        /// <summary>
+        /// Fake long operation
+        /// </summary>
+        private void IncreaseNumberInner()
+        {
+            InProcess = true;
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                Number = i;                
+            }
+            InProcess = false;
+        }
+        
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public AsyncCaller()
+        {
+            Number = 0;
+        }
+
+        /// <summary>
+        /// Public async call of fake long operation
+        /// </summary>
+        public async void IncreaseNumber()
+        {
+            //Cal async as function
+            await Task.Run(() => IncreaseNumberInner());
+        }
+    }
+}
---
+++ AsyncAwaitCoreTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitCoreTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create object
+            var caller = new AsyncCaller();
+            Console.WriteLine("Run!");
+            //Call async fake long operation
+            caller.IncreaseNumber();
+            //Call async loging to console          
+            PrintNames(caller);
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Async log curent index of log operation
+        /// </summary>
+        /// <param name="caller"></param>
+        static async void PrintNames(AsyncCaller caller)
+        {
+            //Lamda call example
+            await Task.Run(() =>
+            {               
+                while (caller.InProcess)
+                {                  
+                    Console.WriteLine($"Number = {caller.Number}");
+                    Thread.Sleep(500);                   
+                }
+                Console.WriteLine("Enough!");
+            });
+        }
+    }
+}
